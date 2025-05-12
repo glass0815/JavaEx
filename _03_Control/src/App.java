06f74bf7eed634e13aa276a050516c6599f30d76
SJFullStack <uri0815@gmail.com>
2025-05-12 15:19:43 +0900

    첫번째실습습

---
+++ _03_Control/src/App.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+
+        if(score >= 90)
+            System.out.println("A학점");
+        
+        // if(score >= 90){
+        //     System.out.println("A학점");
+        // }
+
+        sc.close();
+    }
+}
